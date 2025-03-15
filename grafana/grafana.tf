97440198a990885149285a6c7a8dfa69ddd24d94
tyagipurshottam <purshottam72@gmail.com>
2025-03-15 15:43:36 +0530

    Update grafana.tf

--- grafana/grafana.tf
+++ grafana/grafana.tf
@@ -8,9 +8,6 @@ resource "helm_release" "grafana" {
   provisioner "local-exec" {
     command = <<EOT
       kubectl apply -f kube-bench.yaml;
-      sleep 30;
-      kubectl logs `kubectl get po  | awk '{print $1}' | tail -n 1 ` >> kube-bench.txt;
-      sleep 10;
     EOT
   
   }
